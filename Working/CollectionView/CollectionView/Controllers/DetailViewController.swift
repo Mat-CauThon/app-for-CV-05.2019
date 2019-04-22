4dd0acb5e4344cdd95fb4d239e9bfd4146ee49f8
Роман <30390533+Mat-CauThon@users.noreply.github.com>
2019-04-22 23:24:47 +0300

    Add files via upload

--- Working/CollectionView/CollectionView/Controllers/DetailViewController.swift
+++ Working/CollectionView/CollectionView/Controllers/DetailViewController.swift
@@ -12,27 +12,10 @@ class DetailViewController: UIViewController {
 
     
     var selection: Skill!
-    @IBOutlet weak var navigationItemDetail: UINavigationItem!
+    
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet private weak var detailsLabel: UILabel!
-    @IBOutlet weak var descLabel: UITextField!
-    private var backButton: UIBarButtonItem!
-    private var backButtonDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: Selector(("doneAction")))
-    
-    
-    @IBAction func setEditLabel(_ sender: Any) {
-        descLabel.isEnabled = true
-        backButton = navigationItemDetail.leftBarButtonItem
-        navigationItemDetail.leftBarButtonItem = backButtonDone
-        navigationItemDetail.rightBarButtonItem?.isEnabled = false
-        
-    }
-    
-    @IBAction func doneAction() {
-        navigationItemDetail.leftBarButtonItem = backButton
-        navigationItemDetail.rightBarButtonItem?.isEnabled = true
-        descLabel.isEnabled = false
-    }
+   
 
 
     override func viewDidLoad() {
